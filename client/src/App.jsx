cdb40ccc89fe486818c067ab9995339eac590607
saiklonee <saiprithvitech@gmail.com>
2026-08-13 16:46:22 +0530

    added new images

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,25 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./components/system/Header";
+import { ReactLenis } from "lenis/react";
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
 
 const App = () => {
+  const lenisRef = useRef();
+
+  useEffect(() => {
+    function update(time) {
+      lenisRef.current?.lenis?.raf(time * 1000);
+    }
+
+    gsap.ticker.add(update);
+
+    return () => gsap.ticker.remove(update);
+  }, []);
   return (
     <>
+      <ReactLenis root options={{ autoRaf: false }} ref={lenisRef} />
       <Header />
       <main className="w-full h-full scrollbar-none">
         <Routes>
--- client/src/components/system/Header.jsx
+++ client/src/components/system/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="fixed top-0 w-full p-5 flex justify-between items-center">
+    <header className="fixed top-0 w-full p-3 flex justify-between items-center px-10">
       <div>Home</div>
       <div className="h-full">
         <img className="w-15" src="/logos/logo.png" alt="" />
